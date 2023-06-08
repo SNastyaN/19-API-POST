13bf8dcce3d8749f88d9a314c30fb069dbfd8efe
Anastasiia Sedalishcheva <ffttyyll@gmail.com>
2023-06-08 17:16:06 +0900

    up

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let postButton = document.querySelector('.btn');
+let massagesList = document.querySelector('.container');
 postButton.onclick =function(e){
       let user ={
             title: document.querySelector('#title').value,
@@ -6,26 +7,24 @@ postButton.onclick =function(e){
         }
         console.log(user);
       
-        let massagesList = document.querySelector('.container');
+  
   fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: JSON.stringify(user),
     headers: {
       "Content-type": "application/json; charset=UTF-8"
     }
-  })
-  
-  .then(response => response.json())
-  .then(json => {
-    json.forEach((elem) => {
-          let div = document.createElement('div');
-          div.classList.add('massage');
-          div.innerHTML =`<p>Заголовок:</p>
-          <p>${elem.title}</p><p>Сообщение:</p>
-          <p>${elem.body}</p>
-          `;
-          massagesList.append(div);
+  })  
+    .then(response => response.json())
+    .then(user.forEach(element => {
+      let div = document.createElement('div');
+      div.classList.add('massage');
+      div.innerHTML =`<p>${element.title}</p>
+      <p>${element.body}</p>
+      `;
+      massagesList.append(div);
 
-    }) 
-  
-})}
+
+      
+    }))
+}
